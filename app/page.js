891dd383796a78375fff0cf7ee2928d512ddae98
seuedaltaayira111 <hamdansaudaltayarah@gmail.com>
2026-08-10 17:34:23 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -21,6 +21,8 @@ export default function Home() {
     { id: 'vendors', label: erp.tr.vendors, show: erp.userProfile.is_admin || erp.userProfile.can_access_invoices },
     { id: 'packages', label: erp.tr.packages, show: erp.userProfile.is_admin || erp.userProfile.can_access_invoices },
     { id: 'branches', label: erp.tr.branches, show: erp.userProfile.is_admin || erp.userProfile.can_access_settings },
+    { id: 'contract', label: 'Corporate Contract', show: erp.userProfile.is_admin || erp.userProfile.can_access_invoices },
+    { id: 'offer', label: 'Corporate Offer', show: erp.userProfile.is_admin || erp.userProfile.can_access_invoices },
     { id: 'bank', label: erp.tr.bank, show: erp.userProfile.is_admin || erp.userProfile.can_access_bank },
     { id: 'invest', label: erp.tr.invest, show: erp.userProfile.is_admin || erp.userProfile.can_access_bank },
     { id: 'hr', label: erp.tr.hr, show: erp.userProfile.is_admin || erp.userProfile.can_access_hr },
@@ -45,63 +47,63 @@ export default function Home() {
         settleForm={erp.settleForm} setSettleForm={erp.setSettleForm} handleSettlePayment={erp.handleSettlePayment}
         refundForm={erp.refundForm} setRefundForm={erp.setRefundForm} handleRefund={erp.handleRefund}
         previewHTML={erp.previewHTML}
+        downloadPDF={erp.downloadPDF}
       >
         <ERPViews 
-          // Core Data & State
           page={erp.page} data={erp.data} tr={erp.tr} today={erp.today}
           ledgerCustId={erp.ledgerCustId} setLedgerCustId={erp.setLedgerCustId}
           
-          // Invoices
           invForm={erp.invForm} setInvForm={erp.setInvForm} 
           handleCreateInvoice={erp.handleCreateInvoice} downloadPDF={erp.downloadPDF}
-          printInvoice={erp.printInvoice} exportCSV={erp.exportCSV} exportToExcel={erp.exportToExcel} 
+          printInvoice={erp.printInvoice} exportToExcel={erp.exportToExcel} 
           search={erp.search} setSearch={erp.setSearch} 
           tblPage={erp.tblPage} setTblPage={erp.setTblPage} 
           payFilter={erp.payFilter} setPayFilter={erp.setPayFilter}
           handleEditInvoice={erp.handleEditInvoice} 
           handleDeleteInvoice={erp.handleDeleteInvoice} 
           openRefundModal={erp.openRefundModal} openPreview={erp.openPreview}
+          openSettleModal={erp.openSettleModal} handleQuickSettle={erp.handleQuickSettle}
           editInvId={erp.editInvId}
           
-          // Customer & Corp & Creditors
           handleAddEditCust={erp.handleAddEditCust} custForm={erp.custForm} setCustForm={erp.setCustForm} handleEditCust={erp.handleEditCust} editCustId={erp.editCustId}
           handleAddEditCorp={erp.handleAddEditCorp} corpForm={erp.corpForm} setCorpForm={erp.setCorpForm} handleEditCorp={erp.handleEditCorp} editCorpId={erp.editCorpId}
           handleAddEditCred={erp.handleAddEditCred} creditorForm={erp.creditorForm} setCreditorForm={erp.setCreditorForm} handleEditCred={erp.handleEditCred} editCredId={erp.editCredId}
           
-          // Vendors & Packages & Branches
           handleAddEditVend={erp.handleAddEditVend} vendorForm={erp.vendorForm} setVendorForm={erp.setVendorForm} handleEditVend={erp.handleEditVend} editVendId={erp.editVendId}
           handleAddEditPkg={erp.handleAddEditPkg} pkgForm={erp.pkgForm} setPkgForm={erp.setPkgForm} handleEditPkg={erp.handleEditPkg} editPkgId={erp.editPkgId}
           handleAddEditBrn={erp.handleAddEditBrn} brnForm={erp.brnForm} setBrnForm={erp.setBrnForm} handleEditBrn={erp.handleEditBrn} editBrnId={erp.editBrnId}
           
-          // HR & Employees & Services & Dynamic Expenses
           handleAddEditEmp={erp.handleAddEditEmp} empForm={erp.empForm} setEmpForm={erp.setEmpForm} handleEditEmp={erp.handleEditEmp} editEmpId={erp.editEmpId}
           handleAddEditSrv={erp.handleAddEditSrv} srvForm={erp.srvForm} setSrvForm={erp.setSrvForm} handleEditSrv={erp.handleEditSrv} editSrvId={erp.editSrvId}
           handlePaySalary={erp.handlePaySalary} 
           expForm={erp.expForm} setExpForm={erp.setExpForm} 
+          editExpId={erp.editExpId} setEditExpId={erp.setEditExpId}
           handleAddExpItem={erp.handleAddExpItem} 
           handleRemoveExpItem={erp.handleRemoveExpItem} 
           handleExpItemChange={erp.handleExpItemChange} 
           handleAddExpense={erp.handleAddExpense}
+          handleEditExpense={erp.handleEditExpense} 
+          handleDeleteExpense={erp.handleDeleteExpense} 
+          handlePreviewExpense={erp.handlePreviewExpense}
           
-          // Portals & Investors & Transfers
           handleAddPortal={erp.handleAddPortal} portalForm={erp.portalForm} setPortalForm={erp.setPortalForm}
           handleAddInvestment={erp.handleAddInvestment} investForm={erp.investForm} setInvestForm={erp.setInvestForm}
           handleRecharge={erp.handleRecharge} handleTransfer={erp.handleTransfer} 
           transferForm={erp.transferForm} setTransferForm={erp.setTransferForm}
           
-          // Users & Settings
           handleAddUser={erp.handleAddUser} handleEditUser={erp.handleEditUser} handleUpdateUser={erp.handleUpdateUser} userForm={erp.userForm} setUserForm={erp.setUserForm} editUserId={erp.editUserId}
           handleSaveSettings={erp.handleSaveSettings} handleLogoUpload={erp.handleLogoUpload} 
           setForm={erp.setForm} setSetForm={erp.setSetForm} 
           
-          // Reports & Statements
           repDate={erp.repDate} setRepDate={erp.setRepDate} 
           reportTab={erp.reportTab} setReportTab={erp.setReportTab} 
           statementTab={erp.statementTab} setStatementTab={erp.setStatementTab}
           filterData={erp.filterData}
           
-          // Global Actions
           handleDelete={erp.handleDelete}
+
+          contractCorpName={erp.contractCorpName} setContractCorpName={erp.setContractCorpName}
+          handleGenerateContract={erp.handleGenerateContract} handleGenerateOffer={erp.handleGenerateOffer}
         />
       </ERPLayout>
     </>
