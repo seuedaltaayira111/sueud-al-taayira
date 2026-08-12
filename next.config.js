55d7b2a39f593e8fc349dc7c4d03a8078e3fac17
seuedaltaayira111 <hamdansaudaltayarah@gmail.com>
2026-08-12 19:54:23 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -10,4 +11,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
