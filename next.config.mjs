dfc32a20511b7263bf3504a70ab5c546fdf7f299
seuedaltaayira111 <hamdansaudaltayarah@gmail.com>
2026-08-12 19:10:47 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
