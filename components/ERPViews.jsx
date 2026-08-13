d8e840a06ff2a7cebf4944eea7708d0fbba82a81
seuedaltaayira111 <hamdansaudaltayarah@gmail.com>
2026-08-13 19:23:40 +0300

    Update ERPViews.jsx

--- components/ERPViews.jsx
+++ components/ERPViews.jsx
@@ -6,6 +6,7 @@ import ERPViewsAdmin from './views/ERPViewsAdmin';
 import ERPViewsSystem from './views/ERPViewsSystem';
 import ERPViewsAdvanced from './views/ERPViewsAdvanced';
 import ERPViewsEnterprise from './views/ERPViewsEnterprise';
+import ERPViewsPro from './views/ERPViewsPro';
 
 export default function ERPViews(props) {
   const { page } = props;
@@ -15,6 +16,7 @@ export default function ERPViews(props) {
   const isSystemPage = ['users', 'settings', 'reports', 'audit', 'statements', 'contract', 'offer', 'superadmin', 'profile', 'profitability'].includes(page);
   const isAdvancedPage = ['quotations', 'ai_dashboard', 'hr_advanced'].includes(page);
   const isEnterprisePage = ['credit_limits', 'supplier_statement', 'multi_branch'].includes(page);
+  const isProPage = ['customer_statement', 'recurring_invoices', 'expense_approval', 'notifications'].includes(page);
 
   return (
     <>
@@ -23,6 +25,7 @@ export default function ERPViews(props) {
       {isSystemPage && <ERPViewsSystem {...props} />}
       {isAdvancedPage && <ERPViewsAdvanced {...props} />}
       {isEnterprisePage && <ERPViewsEnterprise {...props} />}
+      {isProPage && <ERPViewsPro {...props} />}
     </>
   );
 }
